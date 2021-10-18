d92dba7e462c9b1970fe16e9aba0bd2d7e9c5522
moise <mose.ns@hotmail.com>
2021-10-18 15:00:52 +0200

    add to cart functionality

--- sneakers/src/App.jsx
+++ sneakers/src/App.jsx
@@ -9,17 +9,42 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 function App() {
 
   const [isCartActive, setIsCartActive] = useState(false);
+  const [cart, setCart] = useState([]);
+  const [prodNumber, setProdNumber] = useState(0)
 
   const activeCart = () => {
     setIsCartActive(!isCartActive);
   }
 
+  //Add to cart functionality: Verify if an item is or 
+  //not in the cart before adding it
+  const addTocart = (product, prodNumber) => {
+    const exist = cart.find(el => el.id === product.id);
+    if (exist) {
+      setCart(
+        cart.map(el => (
+          el.id === product.id ? { ...exist, qty: exist.qty + 1 } : el
+        ))
+      );
+    } else {
+      setCart([...cart, {...product, qty: 1} ]);
+    }
+
+    increaseProdNum(prodNumber)
+  }
+
+  const increaseProdNum = (prodNumber) => {
+    setProdNumber(prodNumber => prodNumber + 1);
+  }
+
+  
+
 
   return (
     <Router>
-      <Nav activeCart={activeCart} isCartActive={isCartActive} />
+      <Nav activeCart={activeCart} isCartActive={isCartActive} addTocart={addTocart} cart={cart} />
       <Route>
-        <Product />
+        <Product addTocart={addTocart} prodNumber={prodNumber} />
       </Route>
     </Router>
   )
--- sneakers/src/components/cart/Cart.jsx
+++ sneakers/src/components/cart/Cart.jsx
@@ -1,34 +1,57 @@
-import './cart.scss'
-import effacer from '../../../public/images/effacer.svg'
-import Button from '../btnComponent/Button';
+import "./cart.scss";
+import effacer from "../../../public/images/effacer.svg";
+import Button from "../btnComponent/Button";
 
+const Cart = ({ isCartActive, addTocart, cart }) => {
+  const totalPrice = cart.reduce(
+    (prevVal, currentVal) => prevVal + currentVal.qty * currentVal.price,
+    0
+  );
 
-const Cart = ({ isCartActive }) => {
-  
   return (
-    <div className={isCartActive ? 'cart-container isActive' : 'cart-container'}>
+    <div
+      className={isCartActive ? "cart-container isActive" : "cart-container"}
+    >
       <header>
-        <h2 className='cart-title'>Cart</h2>
-        <hr className='cart-hr' />
+        <h2 className="cart-title">Cart</h2>
+        <hr className="cart-hr" />
       </header>
       <section>
-        <div className='cart-message'><p>Your Cart is empty.</p></div>
-        <div className='added-product'>
-          <div className='cart-image'>
-            <img src="" alt="" />
-          </div>
-          <div className='cart-descript'>
-            <p>Fall limited edition</p>
-            <p>$125.00 <span className='article-of-number'> x 3</span> <span className='cart-prices'>$375.00</span> </p>
-          </div>
-          <div className='cart-delete'>
-            <img src={effacer} alt="delete-cart" />
-          </div>
+        <div
+          className={
+            cart.length === 0 ? "cart-message isVisible" : "cart-message"
+          }
+        >
+          <p>Your Cart is empty.</p>
         </div>
-        <Button type='button' className='checkout-btn' text='Checkout' />
+        {cart.map((prod) => (
+          <>
+            <div className="added-product" key={prod.id}>
+              <div className="cart-image">
+                <img src={prod.image} alt="" />
+              </div>
+              <div className="cart-descript">
+                <p>{prod.title}</p>
+                <p>
+                  ${prod.price - (prod.price * 50) / 100}
+                  <span className="article-of-number"> x {prod.qty}</span>
+                  <span className="cart-prices">
+                    {' '}${totalPrice - (totalPrice * 50) / 100}.00
+                  </span>
+                </p>
+              </div>
+              <div className="cart-delete">
+                <img src={effacer} alt="delete-cart" />
+              </div>
+            </div>
+          </>
+        ))}
+        {cart.length === 1 && (
+          <Button type="button" className="checkout-btn" text="Checkout" />
+        )}
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
